e8abdff572c9452f9efea5e54a084117e79b4df0
zvhir <zahiruddin@synapse.my>
2023-01-30 20:59:14 +0800

    edit pub in lib/wow

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -269,16 +269,16 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/counterup/counterup.min.js"></script>
-    <script src="lib/parallax/parallax.min.js"></script>
-    <script src="lib/isotope/isotope.pkgd.min.js"></script>
-    <script src="lib/lightbox/js/lightbox.min.js"></script>
+    <script src="/public/lib/wow/wow.min.js"></script>
+    <script src="/public/lib/easing/easing.min.js"></script>
+    <script src="/public/lib/waypoints/waypoints.min.js"></script>
+    <script src="/public/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="/public/lib/counterup/counterup.min.js"></script>
+    <script src="/public/lib/parallax/parallax.min.js"></script>
+    <script src="/public/lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="/public/lib/lightbox/js/lightbox.min.js"></script>
 
-    <script src="js/main.js"></script>
+    <script src="/public/js/main.js"></script>
 
     <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
