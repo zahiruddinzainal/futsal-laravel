e810adeb179a0c0505f0b5beeb1012e1bef7d59e
zvhir <zahiruddin@synapse.my>
2023-01-31 20:41:17 +0800

    add text

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,7 +61,7 @@
                             </button>
                         </div>
                         <p class="text-center mt-5">
-                            <a href="/login">Login to your Account</a>
+                            Already have account? <a href="/login">Login here</a>
                             {{-- <a href="/forget-password">Forgot your password?</a> --}}
                         </p>
                     </div>
